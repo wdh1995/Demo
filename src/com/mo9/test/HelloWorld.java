aa91cadbebf22fcae34f88c634d9bc4a03c04232
wangdh <dhwang@mo9.com>
2017-01-10 10:31:26 +0800

    first commit

---
+++ src/com/mo9/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mo9.test;
+
+
+/**
+ * Created by dhwang on 2017/1/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("day two mo9");
+    }
+}
